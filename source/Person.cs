7a48cfe984c46a225498f1955d67e3648e8980ce
E0001276 <angel.javier1@profuturo.com.mx>
2026-02-27 13:38:19 -0600

    Archivo Person.cs actualizado

--- source/Person.cs
+++ source/Person.cs
@@ -8,5 +8,6 @@ public class Person
   public int Age { get; set; }
   public string Country { get; set; }
 	public string City { get; set; }
-	public string Street {get; set; }
+	public string Street { get; set; }
+	public string Number { get; set; }
 }
