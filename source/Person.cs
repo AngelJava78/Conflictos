3fe09a200c3c23cfa71d495912a5a5db5b1e1388
E0001276 <angel.javier1@profuturo.com.mx>
2026-02-27 13:11:13 -0600

    Person.cs actaulizado

--- source/Person.cs
+++ source/Person.cs
@@ -3,8 +3,9 @@ public class Person
 	public long Id { get; set; }
 	public string Name { get; set; }
 	public string Lastname { get; set; }
-    public DateTime BirthDate { get; set; }
+  public DateTime BirthDate { get; set; }
 	public string Email { get; set; }
-    public int Age { get; set; }
-    public string Country { get; set; }
+  public int Age { get; set; }
+  public string Country { get; set; }
+	public string City { get; set; }
 }
