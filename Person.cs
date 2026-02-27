2f8ebe87881f0c0e32d45ba12aedac7a93663612
E0001276 <angel.javier1@profuturo.com.mx>
2026-02-27 12:18:23 -0600

    Se cambió Apellidos y Correo

--- Person.cs
+++ Person.cs
@@ -2,6 +2,6 @@ public class Person
 {
 	public long Id { get; set; }
 	public string Name { get; set; }
-	public string Lastname { get; set; }
-	public string Email { get; set; }
+	public string Apellidos { get; set; }
+	public string Correo { get; set; }
 }
