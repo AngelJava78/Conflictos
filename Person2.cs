03674587d7644596b429a1b44d8bca635ce1715f
E0001276 <angel.javier1@profuturo.com.mx>
2026-02-27 12:50:37 -0600

    Se agregaron varios archivos .cs

---
+++ Person2.cs
@@ -0,0 +1,10 @@
+public class Person
+{
+	public long Id { get; set; }
+	public string Name { get; set; }
+	public string Lastname { get; set; }
+    public DateTime BirthDate { get; set; }
+	public string Email { get; set; }
+    public int Age { get; set; }
+    public string Country { get; set; }
+}
